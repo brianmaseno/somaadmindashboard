0e071f6390f8f7793f29a4c75483dfc4fc34d300
alekiie <amnjogu78@gmail.com>
2024-07-11 10:23:09 +0300

    Some changes and removed unusable code

--- src/components/AdminDashboard/ViewQuestions.jsx
+++ src/components/AdminDashboard/ViewQuestions.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import "../../css/ViewQuestions.css"; // Adjust the path as necessary
 
 const ViewQuestions = () => {
