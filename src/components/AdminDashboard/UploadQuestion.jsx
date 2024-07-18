fc8b69d11ebbe8140425a89c4f0e7edcffa88144
iankala <iankala12814@gmail.com>
2024-07-18 10:54:40 +0300

    Media querry updated

--- src/components/AdminDashboard/UploadQuestion.jsx
+++ src/components/AdminDashboard/UploadQuestion.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../../css/UploadQuestion.css";
+import "../../css/responsive.css";
 import axios from "axios";
 
 const UploadQuestion = () => {
--- src/components/AdminDashboard/ViewQuestions.jsx
+++ src/components/AdminDashboard/ViewQuestions.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "../../css/ViewQuestions.css";
+import "../../css/responsive.css";
 
 const ViewQuestions = () => {
   const [grade, setGrade] = useState("");
